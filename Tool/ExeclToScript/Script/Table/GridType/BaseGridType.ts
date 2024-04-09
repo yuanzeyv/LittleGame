9c1fb1103c77927cd0f18f25c87969b628ffb8d0
root <846757032@qq.com>
2024-04-09 13:58:32 +0800

    *更新了游戏文件

--- Tool/ExeclToScript/Script/Table/GridType/BaseGridType.ts
+++ Tool/ExeclToScript/Script/Table/GridType/BaseGridType.ts
@@ -1,6 +1,6 @@
 import { AllType } from "../Define";
 export abstract class BaseGridType{
-    public abstract AnalysisType(str:string):boolean;
+public abstract AnalysisType(str:string):boolean;
     public abstract Prase(str:string):AllType|undefined;
     public abstract TypeLenth():number;
     public abstract getTypeForTs():string;
